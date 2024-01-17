e8e065769ce2e63d7d6142c688d5cadbdc2687b6
faridabderrahmani <abderrahmani@yandex.ru>
2024-01-17 07:12:04 +0100

    1st try

--- src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
+++ src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
@@ -3,7 +3,24 @@ package com.epam.mjc.stage0;
 import com.epam.mjc.stage0.utils.Fridge;
 
 public class FridgeAlgorithm {
+    public static void main(String[] args) {
+        Fridge fridge = new Fridge() {
+            @Override
+            public void close() {
 
+            }
+
+            @Override
+            public void open() {
+
+            }
+
+            @Override
+            public void getMilk() {
+
+            }
+        };
+    }
     /**
      * This is a first task in this module. You need to get milk from a fridge =)
      * <p>
@@ -12,5 +29,8 @@ public class FridgeAlgorithm {
      */
     public void fridgeAlgorithm(Fridge fridge) {
 
+        fridge.open();
+        fridge.getMilk();
+        fridge.close();
     }
 }
